4b72a2c33b78c97b87392f2b15e00405d34a5e80
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-08-24 04:13:11 +0600

    init commit

---
+++ NoteApp.ios/NoteApp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp_iosApp.swift
+//  NoteApp.ios
+//
+//  Created by Hossain Muktar on 24/8/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
